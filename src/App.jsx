80864d1c2d13ca68835558bf481b252b7af911f5
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-05 17:01:19 -0400

    feat(app): built in bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ function App() {
   return (
       <div className="container-fluid bg-primary vh-100 text-center">
         <div className="row vh-100 justify-content-center align-items-center">
-          <div id="quote-box" className="col-3 bg-light h-25">
+          <div id="quote-box" className="col-3 h-25 card p-4">
+            <div className="card-body">
+              <figure>
+                <blockquote className="blockquote">
+                  <h1 id="text" className="card-title">...You're Broke!!</h1>
+                </blockquote>
+                <figcaption id="author" className="card-subtitle blockquote-footer text-end">
+                  Rather not say
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card-footer d-flex justify-content-between">
+              <a id="tweet-quote" className="btn btn-primary" href="https://twitter.com/intent/tweet?text=testing">
+                <i className="bi bi-twitter"></i>
+              </a>
+              <button id="new-quote" className="btn btn-primary">Random</button> 
+            </div>
           </div>
         </div>
       </div>
@@ -10,3 +26,4 @@ function App() {
 }
 
 export default App
+
